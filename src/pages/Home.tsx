d252518b7a86e045544096b9812b1bf2507cd4cf
Pedro Zago <pedro.camargozago@hotmail.com.br>
2022-07-12 12:49:28 -0300

    feat(func): challenge requirements implementation

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,36 @@
-import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, View } from "react-native";
 
-import { Header } from '../components/Header';
-import { Task, TasksList } from '../components/TasksList';
-import { TodoInput } from '../components/TodoInput';
+import { Header } from "../components/Header";
+import { Task, TasksList } from "../components/TasksList";
+import { TodoInput } from "../components/TodoInput";
 
 export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    setTasks((oldArray) => [
+      ...oldArray,
+      { id: new Date().getTime(), title: newTaskTitle, done: false },
+    ]);
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    const updatedTask = [...tasks];
+    const taskExists = updatedTask.find((task) => task.id === id);
+
+    if (taskExists) {
+      taskExists.done = !taskExists.done;
+
+      setTasks(updatedTask);
+    }
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    const updatedTask = [...tasks];
+    const filteredTask = updatedTask.filter((task) => task.id !== id);
+
+    setTasks(filteredTask);
   }
 
   return (
@@ -26,18 +39,18 @@ export function Home() {
 
       <TodoInput addTask={handleAddTask} />
 
-      <TasksList 
-        tasks={tasks} 
+      <TasksList
+        tasks={tasks}
         toggleTaskDone={handleToggleTaskDone}
-        removeTask={handleRemoveTask} 
+        removeTask={handleRemoveTask}
       />
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#EBEBEB'
-  }
-})
+    backgroundColor: "#EBEBEB",
+  },
+});
